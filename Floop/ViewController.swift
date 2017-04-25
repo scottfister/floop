04af3f5248949beaf4935a51bd21c6884c63de76
Scott FIster <scottfister@gmail.com>
2017-04-24 20:32:19 -0700

    Initial Commit

---
+++ Floop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Floop
+//
+//  Created by Scott Fister on 4/24/17.
+//  Copyright © 2017 Scott Fister. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
